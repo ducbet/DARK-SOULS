5878f47b06976a073346564c64403bec84d6fe7f
ducbet <ducbet@users.noreply.github.com>
2022-09-04 17:34:30 +0900

    ep007-Player-FALLING-and-Fall-Detection

---
+++ Assets/Scripts/Animator/ResetBoolsOnStateExit.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetBoolsOnStateExit : StateMachineBehaviour
+{
+    public string[] defaultTrueBoolNames;
+    public string[] defaultFalseBoolNames;
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        foreach (var boolName in defaultTrueBoolNames)
+        {
+            animator.SetBool(boolName, true);
+        }
+
+        foreach (var boolName in defaultFalseBoolNames)
+        {
+            animator.SetBool(boolName, false);
+        }
+    }
+}
