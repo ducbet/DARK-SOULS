50bd5b72ede72ceee7813b13228303ce5c1b91d9
ducbet <ducbet@users.noreply.github.com>
2022-09-06 23:35:10 +0900

    ep009-Re-add-namespace

--- Assets/Scripts/AnimatorManager.cs
+++ Assets/Scripts/AnimatorManager.cs
@@ -1,70 +1,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
-public class AnimatorManager : MonoBehaviour
+namespace TMD
 {
-    [HideInInspector] public string movementYParam = "MovementY";
-    [HideInInspector] public string isInteractingParam = "IsInteracting";
-    [HideInInspector] public string isUsingRootMotionParam = "IsUsingRootMotion";
-    [HideInInspector] public string isIgnoreYAxisRootMotionParam = "IsIgnoreYAxisRootMotion";
-    [HideInInspector] public string isGroundParam = "IsGround";
-
-    [HideInInspector] public string fallingAnimation = "Falling";
-    [HideInInspector] public string landingAnimation = "Landing";
-    [HideInInspector] public string rollAnimation = "Roll";
-    [HideInInspector] public string dodgeBackAnimation = "Dodge Back";
-
-    private float fadeLength = 0.2f;
-
-    [HideInInspector] public Vector3 deltaPosition = Vector3.zero;
-
-    private Animator animator;
-    private Dictionary<string, int> animationHash = new Dictionary<string, int>();
-
-    private void Awake()
+    [RequireComponent(typeof(Animator))]
+    public class AnimatorManager : MonoBehaviour
     {
-        animator = GetComponent<Animator>();
-    }
+        [HideInInspector] public string movementYParam = "MovementY";
+        [HideInInspector] public string isInteractingParam = "IsInteracting";
+        [HideInInspector] public string isUsingRootMotionParam = "IsUsingRootMotion";
+        [HideInInspector] public string isIgnoreYAxisRootMotionParam = "IsIgnoreYAxisRootMotion";
+        [HideInInspector] public string isGroundParam = "IsGround";
 
-    public void SetFloat(string animationName, float value)
-    {
-        animator.SetFloat(HashString(animationName), value, 0.1f, Time.deltaTime);
-    }
+        [HideInInspector] public string fallingAnimation = "Falling";
+        [HideInInspector] public string landingAnimation = "Landing";
+        [HideInInspector] public string rollAnimation = "Roll";
+        [HideInInspector] public string dodgeBackAnimation = "Dodge Back";
 
-    public void PlayTargetAnimation(string animationName, bool isInteracting = true)
-    {
-        Debug.Log("PlayTargetAnimation animationName: " + animationName + ", isInteracting: " + isInteracting);
-        deltaPosition = Vector3.zero;
-        animator.SetBool(HashString(isInteractingParam), isInteracting);
-        animator.CrossFade(HashString(animationName), fadeLength);
-    }
+        private float fadeLength = 0.2f;
 
-    private int HashString(string animationName)
-    {
-        if (animationHash.ContainsKey(animationName))
+        [HideInInspector] public Vector3 deltaPosition = Vector3.zero;
+
+        private Animator animator;
+        private Dictionary<string, int> animationHash = new Dictionary<string, int>();
+
+        private void Awake()
         {
+            animator = GetComponent<Animator>();
+        }
+
+        public void SetFloat(string animationName, float value)
+        {
+            animator.SetFloat(HashString(animationName), value, 0.1f, Time.deltaTime);
+        }
+
+        public void PlayTargetAnimation(string animationName, bool isInteracting = true)
+        {
+            Debug.Log("PlayTargetAnimation animationName: " + animationName + ", isInteracting: " + isInteracting);
+            deltaPosition = Vector3.zero;
+            animator.SetBool(HashString(isInteractingParam), isInteracting);
+            animator.CrossFade(HashString(animationName), fadeLength);
+        }
+
+        private int HashString(string animationName)
+        {
+            if (animationHash.ContainsKey(animationName))
+            {
+                return animationHash[animationName];
+            }
+            animationHash[animationName] = Animator.StringToHash(animationName);
             return animationHash[animationName];
         }
-        animationHash[animationName] = Animator.StringToHash(animationName);
-        return animationHash[animationName];
-    }
 
-    public void SetBool(string animationName, bool value)
-    {
-        animator.SetBool(animationName, value);
-    }
-
-    public bool GetBool(string boolName)
-    {
-        return animator.GetBool(HashString(boolName));
-    }
-
-    private void OnAnimatorMove()
-    {
-        if (GetBool(isUsingRootMotionParam))
+        public void SetBool(string animationName, bool value)
         {
-            deltaPosition = animator.deltaPosition / Time.deltaTime;
+            animator.SetBool(animationName, value);
+        }
+
+        public bool GetBool(string boolName)
+        {
+            return animator.GetBool(HashString(boolName));
+        }
+
+        private void OnAnimatorMove()
+        {
+            if (GetBool(isUsingRootMotionParam))
+            {
+                deltaPosition = animator.deltaPosition / Time.deltaTime;
+            }
         }
     }
 }
--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,146 +1,149 @@
 using UnityEngine;
 
-public class CameraManager : MonoBehaviour
+namespace TMD
 {
-    public Transform target;
-    [Header("Translation attributes")]
-    public float translationSmoothTime = 0.1f;
-
-    [Header("Rotation attributes")]
-    public float cameraRotationSpeed = 5f;
-    public float minimumVerticalAngle = -5f;
-    public float maximumVerticalAngle = 20f;
-
-    [Header("Handle collision attributes")]
-    public float spherecastThickness = 0.2f;
-    public float zoomSmoothTime = 0.1f;
-
-    private Transform translationPivotTransform;
-    private Transform rotationPivotTransform;
-    private Transform cameraTransform;
-    private InputManager inputManager;
-
-    private Vector3 currentTranslationVelocity = Vector3.zero;
-    private Vector3 currentZoomVelocity = Vector3.zero;
-
-    private Vector3 cameraRotatelAngle;
-    private Vector3 pivotRotateAngle;
-
-    private float defaultCameraDistance;
-    private bool canZoomOut = false;
-
-    public LayerMask collisionLayers;
-    public enum LayerBits
+    public class CameraManager : MonoBehaviour
     {
-        TransparentFX = 1,
-        IgnoreRaycast = 2,
-        Water = 4,
-        UI = 5,
-        Controller = 6,
-        Environment = 7,
-        Ground = 8,
-        Player = 9,
-    }
+        public Transform target;
+        [Header("Translation attributes")]
+        public float translationSmoothTime = 0.1f;
 
-    public enum LayerMasks
-    {
-        TransparentFX = 1 << LayerBits.TransparentFX,
-        IgnoreRaycast = 1 << LayerBits.IgnoreRaycast,
-        Water = 1 << LayerBits.Water,
-        UI = 1 << LayerBits.UI,
-        Controller = 1 << LayerBits.Controller,
-        Environment = 1 << LayerBits.Environment,
-        Ground = 1 << LayerBits.Ground,
-        Player = 1 << LayerBits.Player,
-    }
+        [Header("Rotation attributes")]
+        public float cameraRotationSpeed = 5f;
+        public float minimumVerticalAngle = -5f;
+        public float maximumVerticalAngle = 20f;
 
-    private void Awake()
-    {
-        if (target == null)
+        [Header("Handle collision attributes")]
+        public float spherecastThickness = 0.2f;
+        public float zoomSmoothTime = 0.1f;
+
+        private Transform translationPivotTransform;
+        private Transform rotationPivotTransform;
+        private Transform cameraTransform;
+        private InputManager inputManager;
+
+        private Vector3 currentTranslationVelocity = Vector3.zero;
+        private Vector3 currentZoomVelocity = Vector3.zero;
+
+        private Vector3 cameraRotatelAngle;
+        private Vector3 pivotRotateAngle;
+
+        private float defaultCameraDistance;
+        private bool canZoomOut = false;
+
+        public LayerMask collisionLayers;
+        public enum LayerBits
         {
-            target = FindObjectOfType<PlayerManager>().transform;
+            TransparentFX = 1,
+            IgnoreRaycast = 2,
+            Water = 4,
+            UI = 5,
+            Controller = 6,
+            Environment = 7,
+            Ground = 8,
+            Player = 9,
         }
 
-        cameraTransform = Camera.main.transform;
-        rotationPivotTransform = cameraTransform.parent;
-        translationPivotTransform = rotationPivotTransform.parent;
-
-        cameraRotatelAngle = cameraTransform.localRotation.eulerAngles;
-        pivotRotateAngle = rotationPivotTransform.rotation.eulerAngles; 
-        defaultCameraDistance = cameraTransform.localPosition.magnitude;
-
-        inputManager = target.GetComponent<InputManager>();
-
-        if (collisionLayers == 0)
+        public enum LayerMasks
         {
-            collisionLayers = (int) ~(LayerMasks.TransparentFX | LayerMasks.IgnoreRaycast | LayerMasks.UI | LayerMasks.Controller | LayerMasks.Environment | LayerMasks.Player);
+            TransparentFX = 1 << LayerBits.TransparentFX,
+            IgnoreRaycast = 1 << LayerBits.IgnoreRaycast,
+            Water = 1 << LayerBits.Water,
+            UI = 1 << LayerBits.UI,
+            Controller = 1 << LayerBits.Controller,
+            Environment = 1 << LayerBits.Environment,
+            Ground = 1 << LayerBits.Ground,
+            Player = 1 << LayerBits.Player,
         }
 
-    }
-
-    public void HandleCameraMovement()
-    {
-        HandleTranslation();
-        HandleRotation();
-        HandleCameraCollision();
-    }
-
-    private void HandleTranslation()
-    {
-        // Why SmoothDamp make jittery?
-        // Vector3 newPosition = Vector3.SmoothDamp(translationPivotTransform.position, target.position, ref currentTranslationVelocity, translationSmoothTime);
-        translationPivotTransform.position = target.position;
-    }
-
-    private void HandleRotation()
-    {
-        // rotate camera on X axis: vertical rotation (rotate camera up and down)
-        cameraRotatelAngle.x -= inputManager.cameraRotationX * cameraRotationSpeed * Time.deltaTime;
-        cameraRotatelAngle.x = Mathf.Clamp(cameraRotatelAngle.x, minimumVerticalAngle, maximumVerticalAngle);
-        cameraTransform.localRotation = Quaternion.Euler(cameraRotatelAngle);
-
-        // rotate pivot on Y axis: horizontal rotation (rotate camera around pivot)
-        pivotRotateAngle.y += inputManager.cameraRotationY * cameraRotationSpeed * Time.deltaTime;
-        rotationPivotTransform.rotation = Quaternion.Euler(pivotRotateAngle);
-    }
-
-    private void HandleCameraCollision()
-    {
-        Vector3 direction = cameraTransform.position - rotationPivotTransform.position;
-        Vector3 offset = direction.normalized * 0.5f;  // offset is for the case player is to close to the wall -> can't detect
-        float sphereCastLength = defaultCameraDistance < direction.magnitude ? defaultCameraDistance + 0.5f : (direction + offset).magnitude;
-        RaycastHit hit;
-        if (Physics.SphereCast(rotationPivotTransform.position - offset, spherecastThickness, direction, out hit, sphereCastLength, collisionLayers))
+        private void Awake()
         {
-            // hit.point is not on the cameraTransform-rotationPivotTransform line. It has small offset because the thickness of the SphereCast
-            // If we zoom in to the hit.point, the forward of the camera is not pointing to the player anymore
-            Vector3 preciseHitPointPrecise = rotationPivotTransform.position + Vector3.Project(hit.point - rotationPivotTransform.position, direction);
-            ZoomIn(preciseHitPointPrecise + offset);  // offset is for the camera does not zoom too close to the player
+            if (target == null)
+            {
+                target = FindObjectOfType<PlayerManager>().transform;
+            }
+
+            cameraTransform = Camera.main.transform;
+            rotationPivotTransform = cameraTransform.parent;
+            translationPivotTransform = rotationPivotTransform.parent;
+
+            cameraRotatelAngle = cameraTransform.localRotation.eulerAngles;
+            pivotRotateAngle = rotationPivotTransform.rotation.eulerAngles;
+            defaultCameraDistance = cameraTransform.localPosition.magnitude;
+
+            inputManager = target.GetComponent<InputManager>();
+
+            if (collisionLayers == 0)
+            {
+                collisionLayers = (int)~(LayerMasks.TransparentFX | LayerMasks.IgnoreRaycast | LayerMasks.UI | LayerMasks.Controller | LayerMasks.Environment | LayerMasks.Player);
+            }
+
         }
-        else
-        {
-            ZoomOut(rotationPivotTransform.position + (direction.normalized * defaultCameraDistance));
-        }
-    }
 
-    private void ZoomIn(Vector3 hitPoint)
-    {
-        Vector3 zoomedInCameraPosition = Vector3.SmoothDamp(cameraTransform.position, hitPoint, ref currentZoomVelocity, zoomSmoothTime);
-        cameraTransform.position = zoomedInCameraPosition;
-        canZoomOut = true;
-    }
-
-    private void ZoomOut(Vector3 defaultCameraPosition)
-    {
-        if (!canZoomOut)
+        public void HandleCameraMovement()
         {
-            return;
+            HandleTranslation();
+            HandleRotation();
+            HandleCameraCollision();
         }
-        Vector3 zoomedOutCameraPosition = Vector3.SmoothDamp(cameraTransform.position, defaultCameraPosition, ref currentZoomVelocity, zoomSmoothTime);
-        cameraTransform.position = zoomedOutCameraPosition;
-        if ((defaultCameraPosition - zoomedOutCameraPosition).magnitude < 0.1)
+
+        private void HandleTranslation()
         {
-            canZoomOut = false;
+            // Why SmoothDamp make jittery?
+            // Vector3 newPosition = Vector3.SmoothDamp(translationPivotTransform.position, target.position, ref currentTranslationVelocity, translationSmoothTime);
+            translationPivotTransform.position = target.position;
+        }
+
+        private void HandleRotation()
+        {
+            // rotate camera on X axis: vertical rotation (rotate camera up and down)
+            cameraRotatelAngle.x -= inputManager.cameraRotationX * cameraRotationSpeed * Time.deltaTime;
+            cameraRotatelAngle.x = Mathf.Clamp(cameraRotatelAngle.x, minimumVerticalAngle, maximumVerticalAngle);
+            cameraTransform.localRotation = Quaternion.Euler(cameraRotatelAngle);
+
+            // rotate pivot on Y axis: horizontal rotation (rotate camera around pivot)
+            pivotRotateAngle.y += inputManager.cameraRotationY * cameraRotationSpeed * Time.deltaTime;
+            rotationPivotTransform.rotation = Quaternion.Euler(pivotRotateAngle);
+        }
+
+        private void HandleCameraCollision()
+        {
+            Vector3 direction = cameraTransform.position - rotationPivotTransform.position;
+            Vector3 offset = direction.normalized * 0.5f;  // offset is for the case player is to close to the wall -> can't detect
+            float sphereCastLength = defaultCameraDistance < direction.magnitude ? defaultCameraDistance + 0.5f : (direction + offset).magnitude;
+            RaycastHit hit;
+            if (Physics.SphereCast(rotationPivotTransform.position - offset, spherecastThickness, direction, out hit, sphereCastLength, collisionLayers))
+            {
+                // hit.point is not on the cameraTransform-rotationPivotTransform line. It has small offset because the thickness of the SphereCast
+                // If we zoom in to the hit.point, the forward of the camera is not pointing to the player anymore
+                Vector3 preciseHitPointPrecise = rotationPivotTransform.position + Vector3.Project(hit.point - rotationPivotTransform.position, direction);
+                ZoomIn(preciseHitPointPrecise + offset);  // offset is for the camera does not zoom too close to the player
+            }
+            else
+            {
+                ZoomOut(rotationPivotTransform.position + (direction.normalized * defaultCameraDistance));
+            }
+        }
+
+        private void ZoomIn(Vector3 hitPoint)
+        {
+            Vector3 zoomedInCameraPosition = Vector3.SmoothDamp(cameraTransform.position, hitPoint, ref currentZoomVelocity, zoomSmoothTime);
+            cameraTransform.position = zoomedInCameraPosition;
+            canZoomOut = true;
+        }
+
+        private void ZoomOut(Vector3 defaultCameraPosition)
+        {
+            if (!canZoomOut)
+            {
+                return;
+            }
+            Vector3 zoomedOutCameraPosition = Vector3.SmoothDamp(cameraTransform.position, defaultCameraPosition, ref currentZoomVelocity, zoomSmoothTime);
+            cameraTransform.position = zoomedOutCameraPosition;
+            if ((defaultCameraPosition - zoomedOutCameraPosition).magnitude < 0.1)
+            {
+                canZoomOut = false;
+            }
         }
     }
 }
