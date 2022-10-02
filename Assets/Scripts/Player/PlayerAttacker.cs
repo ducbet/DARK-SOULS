20f7161ce986fddb63b97d7ba2d1d6b545e6ca70
ducbet <ducbet@users.noreply.github.com>
2022-10-02 14:46:04 +0900

    ep012-WEAPON-COMBOS

--- Assets/Scripts/Player/PlayerAttacker.cs
+++ Assets/Scripts/Player/PlayerAttacker.cs
@@ -17,6 +17,13 @@ namespace TMD
         [HideInInspector] public AnimatorManager animatorManager;
         [HideInInspector] public ActionAnimationSelector attackAnimationSelector;
 
+        private string lastAttackName = "";
+        private Dictionary<string, string> comboAttacks = new Dictionary<string, string>{
+            {"straight_sword_oh_light_attack_01", "straight_sword_oh_light_attack_02"},
+            {"straight_sword_oh_light_attack_02", "straight_sword_oh_heavy_attack_01"},
+            {"straight_sword_oh_heavy_attack_01", "straight_sword_oh_heavy_attack_02"},
+            {"straight_sword_oh_heavy_attack_02", "straight_sword_oh_light_attack_01"},
+        };
 
         private void Awake()
         {
@@ -28,14 +35,24 @@ namespace TMD
         public void Attack(bool isHeavyAttack = false)
         {
             string attackAnimationName = GetAttackAnimation(isHeavyAttack);
+            lastAttackName = attackAnimationName;
             if (attackAnimationName != "")
             {
                 animatorManager.PlayTargetAttackAnimation(attackAnimationName);
             }
         }
 
+        public void ClearComboAttack()
+        {
+            lastAttackName = "";
+        }
+
         private string GetAttackAnimation(bool isHeavyAttack)
         {
+            if (lastAttackName != "")
+            {
+                return GetComboAttackAnimation();
+            }
             InventoryManager.HOLDING_ITEM_STATE holdingState = inventoryManager.GetHoldingItemState();
             InventoryManager.ITEM_TYPE rightHandItemType = inventoryManager.GetItemType(inventoryManager.rightHandItem);
             if (isHeavyAttack)
@@ -43,6 +60,14 @@ namespace TMD
                 return attackAnimationSelector.GetAttackAnimation(holdingState, rightHandItemType, "_heavy_attack");
             }
             return attackAnimationSelector.GetAttackAnimation(holdingState, rightHandItemType);
+        }
+        private string GetComboAttackAnimation()
+        {
+            if (comboAttacks.ContainsKey(lastAttackName))
+            {
+                return comboAttacks[lastAttackName];
+            }
+            return "";
         }
     }
 }
